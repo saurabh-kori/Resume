a506cf29a1457978bca743c89b999725a79ac490
saurabh kori <saurabhkori321@gmail.com>
2021-01-07 13:55:20 +0530

    Resume done

---
+++ Resume.js
@@ -0,0 +1,14 @@
+// var navMenu = document.querySelectorAll('nav a'); 
+
+
+// for(var i=0; i<navMenu.length; i++){
+// 	navMenu[i].addEventListener('click', function(event){
+// 		event.preventDefault();
+// 	});
+// }	
+// var navMenu = document.querySelectorAll('nav a'); 
+// console.log("hello");
+
+var progressBar = document.querySelectorAll('.skills-progress > div');
+window.addEventListener('scroll',checkScroll);
+var
